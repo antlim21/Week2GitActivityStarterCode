ef9bd42f25f74e4d60817c23822143cfe3297a8e
antlim21 <116113300+antlim21@users.noreply.github.com>
2023-09-14 11:18:15 -0400

    Update calculator.ts

--- src/calculator.ts
+++ src/calculator.ts
@@ -2,4 +2,16 @@ export class Calculator {
   public add(x: number, y: number) : number {
     return x + y;
   }
+
+  public subtract(x: number, y: number) : number {
+    return x - y;
+  }
+
+  public multiply(x: number, y: number) : number {
+    return x * y;
+  }
+
+  public divide(x: number, y: number) : number {
+    return x / y;
+  }
 };
